c528dfa286386243d29ab7d0f0f2538c562a503a
Loi Nguyen <ducloi221@gmail.com>
2023-01-18 22:17:28 +0700

    Use startify theme

--- config.lua
+++ config.lua
@@ -111,7 +111,7 @@ lvim.builtin.which_key.mappings["bn"] = { ":bn<CR>", "Next" }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerSync
 lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.mode = "startify"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
