6513bdcb0c999f89c9a3878b25577e0c5c1e047e
Loi Nguyen <ducloi221@gmail.com>
2023-01-18 22:39:06 +0700

    Disable core plugins

--- config.lua
+++ config.lua
@@ -12,25 +12,20 @@ lvim.format_on_save = {
   timeout = 1000,
 }
 
--- to disable icons and use a minimalist setup, uncomment the following
-
 lvim.leader = "space"
--- add your own keymapping
+
+-- Mapping normal keys
 lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
 lvim.keys.insert_mode["jj"] = "<esc>"
 lvim.keys.normal_mode["<C-p>"] = "<cmd>:FzfLua files<CR>"
-
 lvim.keys.normal_mode["tk"] = ":tabnext<CR>"
 lvim.keys.normal_mode["tj"] = ":tabprev<CR>"
 lvim.keys.normal_mode["tt"] = ":tabclose<CR>"
 lvim.keys.normal_mode["cn"] = ":cn<CR>"
 lvim.keys.normal_mode["cp"] = ":cp<CR>"
-
 lvim.keys.normal_mode["#"] = "*" -- Quick search next
 
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
-lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- Mapping which key
 lvim.builtin.which_key.mappings["r"] = {
   name = "+Replace",
   r = {
@@ -41,17 +36,6 @@ lvim.builtin.which_key.mappings["r"] = {
     "<cmd>lua require('spectre').open_visual({select_word=true})<CR>",
     "Replace with current word"
   }
-}
-lvim.builtin.which_key.mappings["c"] = {
-  name = "+ChatGPT",
-  c = {
-    "<cmd>:ChatGPT<cr>",
-    "ChatGPT"
-  },
-  a = {
-    "<cmd>:ChatGPTActAs<cr>",
-    "ChatGPT Act As"
-  },
 }
 lvim.builtin.which_key.mappings["n"] = {
   name = "+Node",
@@ -103,10 +87,10 @@ lvim.builtin.which_key.mappings["sk"] = { ":FzfLua keymaps", "Search keymaps" }
 lvim.builtin.which_key.mappings["bb"] = { ":bp<CR>", "Previous" }
 lvim.builtin.which_key.mappings["bn"] = { ":bn<CR>", "Next" }
 
--- -- Change theme settings
--- lvim.colorscheme = "lunar"
+-- Change theme settings
+lvim.colorscheme = "tokyonight"
 
--- After changing plugin config exit and reopen LunarVim, Run :PackerSync
+-- Lvim builtin configuration
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "startify"
 lvim.builtin.terminal.active = true
@@ -114,122 +98,16 @@ lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.bufferline.options.mode = "tabs" -- set to "tabs" to only show tabpages instead
 
--- Custom telescope
-lvim.builtin.telescope.on_config_done = function(telescope)
-  local actions = require("telescope.actions")
-
-  telescope.setup({
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-u>"] = false, -- clear prompt
-          ["<esc>"] = actions.close, -- quit insert mode
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
-        },
-        n = {
-          ["<C-u>"] = false, -- clear prompt
-          ["<esc>"] = actions.close, -- quit insert mode
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
-        }
-      },
-      pickers = {
-        find_files = {
-          find_command = { "rg", "--files", "--glob", "!**/.git/*" }, -- hide git branch
-        }
-      },
-      extensions = {
-        -- fzf = {
-        --   fuzzy = true, -- false will only do exact matching
-        --   override_generic_sorter = true, -- override the generic sorter
-        --   override_file_sorter = true, -- override the file sorter
-        --   -- case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-        --   case_mode = "ignore_case", -- or "ignore_case" or "respect_case"
-        -- },
-      },
-      initial_mode = "insert",
-      layout_strategy = "horizontal",
-      layout_config = {
-        horizontal = {
-          prompt_position = "top",
-          preview_width = 0.55,
-          results_width = 0.8,
-        },
-        vertical = {
-          mirror = false,
-        },
-        width = 0.87,
-        height = 0.80,
-        preview_cutoff = 120,
-      },
-      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-      buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-      color_devicons = true,
-      border = {},
-      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-    },
-  })
-
-  -- any other extensions loading
-  -- telescope.load_extension('fzf')
-end
-
 -- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
 lvim.builtin.treesitter.rainbow.enable = true
 
--- lvim.builtin.treesitter.ignore_install = { "haskell" }
+-- Disable core plugins
+lvim.builtin.telescope.active = false
+lvim.builtin.project.active = false
 
--- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
 
--- --- disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "stylua" },
---   {
---     command = "prettier",
---     extra_args = { "--print-with", "100" },
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     command = "shellcheck",
---     args = { "--severity", "warning" },
---   },
--- }
-
--- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
+-- Additional Plugins
 lvim.plugins = {
   {
     "phaazon/hop.nvim",
@@ -250,21 +128,11 @@ lvim.plugins = {
         debug = false; -- Print debug information
         opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
         post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
-        -- You can use "default_mappings = true" setup option
-        -- Or explicitly set keybindings
-        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
-        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
-        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
       }
     end
   },
   {
     "tpope/vim-surround",
-
-    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-    -- setup = function()
-    --  vim.o.timeoutlen = 500
-    -- end
   },
   {
     "turbio/bracey.vim",
@@ -297,14 +165,9 @@ lvim.plugins = {
     config = function()
       require("gitlinker").setup {
         opts = {
-          -- remote = 'github', -- force the use of a specific remote
-          -- adds current line nr in the url for normal mode
           add_current_line_on_normal_mode = true,
-          -- callback for what to do with the url
           action_callback = require("gitlinker.actions").open_in_browser,
-          -- print the url after performing the action
           print_url = false,
-          -- mapping to call url generation
           mappings = "<leader>gy",
         },
       }
@@ -312,12 +175,8 @@ lvim.plugins = {
     dependencies = "nvim-lua/plenary.nvim",
   },
   {
-    -- You must install glow globally
-    -- https://github.com/charmbracelet/glow
-    -- yay -S glow
     "npxbr/glow.nvim",
     ft = { "markdown" }
-    -- build = "yay -S glow"
   },
   {
     "p00f/nvim-ts-rainbow",
@@ -327,7 +186,6 @@ lvim.plugins = {
     event = "WinScrolled",
     config = function()
       require('neoscroll').setup({
-        -- All these keys will be mapped to their corresponding default scrolling animation
         mappings = { '<C-u>', '<C-d>' },
         hide_cursor = true, -- Hide cursor while scrolling
         stop_eof = true, -- Stop at <EOF> when scrolling downwards
@@ -340,19 +198,6 @@ lvim.plugins = {
       })
     end
   },
-  {
-    "itchyny/vim-cursorword",
-    event = { "BufEnter", "BufNewFile" },
-    config = function()
-      vim.api.nvim_command("augroup user_plugin_cursorword")
-      vim.api.nvim_command("autocmd!")
-      vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
-      vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
-      vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
-      vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
-      vim.api.nvim_command("augroup END")
-    end
-  },
   { "tpope/vim-repeat" },
   { "wakatime/vim-wakatime" },
   { "tpope/vim-projectionist" },
@@ -363,19 +208,12 @@ lvim.plugins = {
     dependencies = { 'nvim-treesitter' },
     opts = {},
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
-    },
-    opts = {},
-  },
   { 'ibhagwan/fzf-lua' },
 }
 
--- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
+vim.g['test#strategy'] = 'vimux'
+
+-- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 -- vim.api.nvim_create_autocmd("FileType", {
 --   pattern = "zsh",
 --   callback = function()
@@ -383,7 +221,3 @@ lvim.plugins = {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
-vim.g['test#strategy'] = 'vimux'
-
--- Disable plugins
--- lvim.builtin.telescope.active = false
