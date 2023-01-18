957b0545df369539e5bf27b1d741bfe42cecc89f
Loi Nguyen <ducloi221@gmail.com>
2023-01-18 23:13:47 +0700

    Update keymap

--- whichkey_mapping.lua
+++ whichkey_mapping.lua
@@ -1,11 +1,17 @@
-lvim.builtin.which_key.mappings["ln"] = { "<cmd>NodeAction<cr>", "Node Action" }
+lvim.builtin.which_key.mappings["lA"] = { "<cmd>NodeAction<cr>", "Node Action" }
 lvim.builtin.which_key.mappings["f"] = {
   name = "+File",
   e = { "<cmd>NvimTreeFindFile<CR>", "Find file in explorer" },
   y = { "<cmd>let @+=expand('%')<CR>", "Copy file path" },
   b = { "<cmd>FzfLua buffers<CR>", "Find buffers" },
-  l = { "<cmd>FzfLua blines<CR>", "Find lines" },
-  f = { "<cmd>FzfLua grep_cword<CR>", "Find grep cword" },
+  o = { "<cmd>FzfLua oldfiles<CR>", "Find old files" },
+}
+lvim.builtin.which_key.mappings["s"] = {
+  name = "+Search",
+  h = { "<cmd>FzfLua help_tags<cr>", "Search help" },
+  k = { "<cmd>FzfLua keymaps<cr>", "Search keymaps" },
+  t = { "<cmd>FzfLua grep_cword<cr>", "Search text" },
+  l = { "<cmd>FzfLua blines<CR>", "Search buffer lines" },
 }
 lvim.builtin.which_key.mappings["gy"] = {
   "<cmd>lua require('gitlinker').get_buf_range_url('n')",
@@ -16,7 +22,8 @@ lvim.builtin.which_key.mappings["t"] = {
   n = { "<cmd>TestNearest<CR>", "Test Nearest" },
   f = { "<cmd>TestFile<CR>", "Test File" },
 }
-lvim.builtin.which_key.mappings["sh"] = { "<cmd>FzfLua help_tags<cr>", "Search help" }
 lvim.builtin.which_key.mappings["sk"] = { "<cmd>FzfLua keymaps<cr>", "Search keymaps" }
+
+
 lvim.builtin.which_key.mappings["bb"] = { "<cmd>bp<CR>", "Previous" }
 lvim.builtin.which_key.mappings["bn"] = { "<cmd>bn<CR>", "Next" }
