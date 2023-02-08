91dbdebb7d21b9137fe4e1504da045a9cdea9e25
Loi Nguyen <ducloi221@gmail.com>
2023-02-08 21:06:23 +0700

    Add treesister text object

--- config.lua
+++ config.lua
@@ -177,4 +177,48 @@ lvim.plugins = {
   { "kevinhwang91/nvim-bqf" }, -- Better quickfix list
   { "ibhagwan/fzf-lua" },
   { "kdheepak/lazygit.nvim" },
+  { "nvim-treesitter/nvim-treesitter-textobjects" }
 }
+
+lvim.builtin.treesitter.on_config_done = function()
+  require 'nvim-treesitter.configs'.setup {
+    textobjects = {
+      select = {
+        enable = true,
+
+        -- Automatically jump forward to textobj, similar to targets.vim
+        lookahead = true,
+
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["af"] = { query = "@function.outer", desc = "Select outer function" },
+          ["if"] = { query = "@function.inner", desc = "Select inner function" },
+          ["ac"] = { query = "@class.outer", desc = "Select outer class" },
+          ["ic"] = { query = "@class.inner", desc = "Select inner class" },
+        },
+        -- You can choose the select mode (default is charwise 'v')
+        --
+        -- Can also be a function which gets passed a table with the keys
+        -- * query_string: eg '@function.inner'
+        -- * method: eg 'v' or 'o'
+        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+        -- mapping query_strings to modes.
+        selection_modes = {
+          ['@parameter.outer'] = 'v', -- charwise
+          ['@function.outer'] = 'V', -- linewise
+          ['@class.outer'] = '<c-v>', -- blockwise
+        },
+        -- If you set this to `true` (default is `false`) then any textobject is
+        -- extended to include preceding or succeeding whitespace. Succeeding
+        -- whitespace has priority in order to act similarly to eg the built-in
+        -- `ap`.
+        --
+        -- Can also be a function which gets passed a table with the keys
+        -- * query_string: eg '@function.inner'
+        -- * selection_mode: eg 'v'
+        -- and should return true of false
+        include_surrounding_whitespace = true,
+      },
+    },
+  }
+end
