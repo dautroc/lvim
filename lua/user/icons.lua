4ef58513bb099b07c6e3eebf146919273fc7477b
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-03-15 19:07:12 +0700

    Add instruction

--- lua/user/icons.lua
+++ lua/user/icons.lua
@@ -1,7 +1,10 @@
--- https://github.com/microsoft/vscode/blob/main/src/vs/base/common/codicons.ts
--- go to the above and then enter <c-v>u<unicode> and the symbold should appear
--- or go here and upload the font file: https://mathew-kurian.github.io/CharacterMap/
--- find more here: https://www.nerdfonts.com/cheat-sheet
+-- [A nerd font](https://github.com/ryanoasis/nerd-fonts)
+-- [codicon](https://github.com/microsoft/vscode-codicons/raw/main/dist/codicon.ttf)
+-- [An emoji font](https://github.com/googlefonts/noto-emoji/blob/main/fonts/NotoColorEmoji.ttf)
+
+-- After moving fonts to `~/.local/share/fonts/`
+-- Run: `$ fc-cache -f -v`
+
 vim.g.use_nerd_icons = true
 if (vim.fn.has "mac" == 1 or vim.g.use_nerd_icons) then
   -- elseif vim.fn.has "mac" == 1 then
