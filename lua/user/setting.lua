c063aef7d0188a6f8687394d1ad8921c845599bf
Loi Nguyen <loi.nguyen@employmenthero.com>
2023-04-04 23:13:55 +0700

    Config zenmode

--- lua/user/setting.lua
+++ lua/user/setting.lua
@@ -1,5 +1,6 @@
 -- vim options
 vim.opt.backup = false                     -- creates a backup file
+vim.opt.foldcolumn = "0"                 -- creates a backup file
 vim.opt.clipboard = "unnamedplus"          -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 2                      -- more space in the neovim command line for displaying messages
 vim.opt.colorcolumn = "99999"              -- fixes indentline for now
